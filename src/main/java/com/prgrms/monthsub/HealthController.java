111b5147a29ad894634fd8d2c03a922d692d671a
eunseo2 <20180972@sungshin.ac.kr>
2021-12-03 23:20:57 +0900

    [MON-80] chore : 코드리뷰 반영

--- src/main/java/com/prgrms/monthsub/HealthController.java
+++ src/main/java/com/prgrms/monthsub/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthyController {
+public class HealthController {
 
     @GetMapping("/health")
     @Operation(hidden = true)
