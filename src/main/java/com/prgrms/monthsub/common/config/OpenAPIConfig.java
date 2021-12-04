ae4fae4e7de6ee02e58f3216b696570485f5af38
Jo-GyuHyeon <kuhyun1993@gmail.com>
2021-12-04 13:05:07 +0900

    [MON-84] feat: SpringDocs JWP 토큰 설정 추가
    yml 파일도 함께 정리하다.
    로그 관리를 위해 local 에서만 hibernate 쿼리를 남기도록 설정하다.

---
+++ src/main/java/com/prgrms/monthsub/common/config/OpenAPIConfig.java
@@ -0,0 +1,62 @@
+package com.prgrms.monthsub.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.Paths;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import java.util.Arrays;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class OpenAPIConfig {
+
+    private static final String PROD = "prod";
+
+    private final JwtConfig jwtConfig;
+
+    private final Environment environment;
+
+    public OpenAPIConfig(JwtConfig jwtConfig, Environment environment) {
+        this.jwtConfig = jwtConfig;
+        this.environment = environment;
+    }
+
+    @Bean
+    public OpenApiCustomiser customOpenAPI(BuildProperties buildProperties) {
+        return openAPI -> {
+            if (!Arrays.asList(environment.getActiveProfiles()).contains(PROD)) {
+                openAPI.info(new Info()
+                        .title("MonthSub API").version(buildProperties.getVersion())
+                        .termsOfService("Sagack")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                    )
+                    .components(
+                        openAPI.getComponents()
+                            .addSecuritySchemes(
+                                "bearer",
+                                new SecurityScheme()
+                                    .type(Type.APIKEY)
+                                    .scheme("bearer").bearerFormat("JWT")
+                                    .in(SecurityScheme.In.HEADER).name(jwtConfig.getHeader())
+                            )
+                    )
+                    .addSecurityItem(
+                        new SecurityRequirement()
+                            .addList("bearer", Arrays.asList("read", "write"))
+                    );
+            } else {
+                openAPI.setComponents(new Components());
+                openAPI.setPaths(new Paths());
+            }
+        };
+    }
+
+}
