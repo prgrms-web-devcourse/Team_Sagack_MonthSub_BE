46edc1dae2b6dc9b1977df652744e1eaa1c04407
youji <dbwlgna98@naver.com>
2021-12-07 15:35:10 +0900

    [MON-118] chore : 좋아요 API 코드 수정

--- src/main/java/com/prgrms/monthsub/module/series/series/app/SeriesLikesService.java
+++ src/main/java/com/prgrms/monthsub/module/series/series/app/SeriesLikesService.java
@@ -4,9 +4,10 @@ import com.prgrms.monthsub.module.series.series.domain.Series;
 import com.prgrms.monthsub.module.series.series.domain.SeriesLikes;
 import com.prgrms.monthsub.module.series.series.domain.SeriesLikes.LikesStatus;
 import com.prgrms.monthsub.module.series.series.dto.SeriesLikesEvent;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class SeriesLikesService {
@@ -16,7 +17,7 @@ public class SeriesLikesService {
     private final SeriesService seriesService;
 
     public SeriesLikesService(SeriesLikesRepository seriesLikesRepository,
-        SeriesService seriesService, SeriesService seriesService1) {
+                              SeriesService seriesService, SeriesService seriesService1) {
         this.seriesLikesRepository = seriesLikesRepository;
         this.seriesService = seriesService1;
     }
@@ -24,23 +25,33 @@ public class SeriesLikesService {
     @Transactional
     public SeriesLikesEvent.Response likesEvent(Long userId, Long seriesId) {
         Optional<SeriesLikes> seriesLikes = seriesLikesRepository.findSeriesLikesByUserIdAndSeriesId(
-            userId, seriesId);
+                userId, seriesId);
         if (seriesLikes.isPresent()) {
             LikesStatus changeStatus = seriesLikes.get().changeLikeStatus();
             seriesLikes.get().getSeries().changeLikesCount(changeStatus);
             return new SeriesLikesEvent.Response(
-                seriesLikesRepository.save(seriesLikes.get()).getId(),
-                String.valueOf(changeStatus)
+                    seriesLikesRepository.save(seriesLikes.get()).getId(),
+                    String.valueOf(changeStatus)
             );
         }
         Series seriesEntity = seriesService.getSeriesById(seriesId);
         seriesEntity.changeLikesCount(LikesStatus.Like);
         SeriesLikes seriesLikesEntity = SeriesLikes.builder().userId(userId).series(seriesEntity)
-            .likesStatus(LikesStatus.Like).build();
+                .likesStatus(LikesStatus.Like).build();
         return new SeriesLikesEvent.Response(
-            seriesLikesRepository.save(seriesLikesEntity).getId(),
-            String.valueOf(LikesStatus.Like)
+                seriesLikesRepository.save(seriesLikesEntity).getId(),
+                String.valueOf(LikesStatus.Like)
         );
     }
 
+    @Transactional
+    public SeriesLikesEvent.Response cancelSeriesLike(Long userId, Long seriesId) {
+        Optional<SeriesLikes> seriesLikes = seriesLikesRepository.findSeriesLikesByUserIdAndSeriesId(
+                userId, seriesId);
+        LikesStatus changeStatus = seriesLikes.get().changeLikeStatus();
+        seriesLikes.get().getSeries().changeLikesCount(changeStatus);
+        return new SeriesLikesEvent.Response(
+                seriesLikesRepository.save(seriesLikes.get()).getId(),
+                String.valueOf(changeStatus));
+    }
 }
