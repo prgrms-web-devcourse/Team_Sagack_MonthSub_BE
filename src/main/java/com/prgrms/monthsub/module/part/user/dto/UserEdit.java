75e14cb9121c0e3c344f9a2c07b5625409e5d870
eunseo2 <20180972@sungshin.ac.kr>
2021-12-08 21:18:00 +0900

    [MON-51] feat : Bean Validation 추가

--- src/main/java/com/prgrms/monthsub/module/part/user/dto/UserEdit.java
+++ src/main/java/com/prgrms/monthsub/module/part/user/dto/UserEdit.java
@@ -1,12 +1,17 @@
 package com.prgrms.monthsub.module.part.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
 
 public class UserEdit {
 
   @Schema(name = "UserEdit.Request")
   public record Request(
+
+    @NotBlank(message = "닉네임이 비어있습니다.")
     String nickName,
+
+    @NotBlank(message = "소개글이 비어있습니다.")
     String profileIntroduce
   ) {}
 
--- src/main/java/com/prgrms/monthsub/module/part/user/dto/UserLogin.java
+++ src/main/java/com/prgrms/monthsub/module/part/user/dto/UserLogin.java
@@ -1,12 +1,17 @@
 package com.prgrms.monthsub.module.part.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
 
 public class UserLogin {
 
   @Schema(name = "UserLogin.Request")
   public record Request(
+
+    @NotBlank(message = "이메일이 비어있습니다.")
     String email,
+
+    @NotBlank(message = "비밀번호가 비어있습니다.")
     String password
   ) {}
 
