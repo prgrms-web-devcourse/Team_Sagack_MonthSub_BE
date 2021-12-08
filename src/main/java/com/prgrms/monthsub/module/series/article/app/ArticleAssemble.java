c41ddcb837ee28ae59bca7d26da501ff9c83eb2f
0923kdh <0923kdh@gmail.com>
2021-12-08 15:06:47 +0900

    [MON-18] feat:  아티클 발행 API 수정 - 코드 리팩토링 과정에서 잘못합쳐진 부분 수정

--- src/main/java/com/prgrms/monthsub/module/series/article/app/ArticleAssemble.java
+++ src/main/java/com/prgrms/monthsub/module/series/article/app/ArticleAssemble.java
@@ -5,55 +5,57 @@ import com.prgrms.monthsub.module.series.article.domain.Article;
 import com.prgrms.monthsub.module.series.article.dto.ArticlePost;
 import com.prgrms.monthsub.module.series.series.app.SeriesService;
 import com.prgrms.monthsub.module.series.series.domain.Series;
-import java.io.IOException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @Transactional(readOnly = true)
 public class ArticleAssemble {
 
-  private final ArticleService articleService;
+    private final ArticleService articleService;
 
-  private final SeriesService seriesService;
+    private final SeriesService seriesService;
 
-  private final ArticleConverter articleConverter;
+    private final ArticleConverter articleConverter;
 
-  public ArticleAssemble(
-    ArticleService articleService,
-    SeriesService seriesService,
-    ArticleConverter articleConverter
-  ) {
-    this.articleService = articleService;
-    this.seriesService = seriesService;
-    this.articleConverter = articleConverter;
-  }
+    public ArticleAssemble(
+            ArticleService articleService,
+            SeriesService seriesService,
+            ArticleConverter articleConverter
+    ) {
+        this.articleService = articleService;
+        this.seriesService = seriesService;
+        this.articleConverter = articleConverter;
+    }
 
-  @Transactional
-  public ArticlePost.Response createArticle(
-    MultipartFile thumbnail,
-    ArticlePost.Request request
-  ) throws IOException {
-    Series series = this.seriesService.getById(request.seriesId());
+    @Transactional
+    public ArticlePost.Response createArticle(
+            MultipartFile thumbnail,
+            ArticlePost.Request request
+    ) throws IOException {
+        Series series = this.seriesService.getById(request.seriesId());
 
-    Long articleCount = this.articleService.countBySeriesId(request.seriesId());
+        Long articleCount = this.articleService.countBySeriesId(request.seriesId());
 
-    Article article = articleConverter.ArticlePostToEntity(
-      series,
-      request,
-      articleCount.intValue() + 1
-    );
+        Article article = articleConverter.ArticlePostToEntity(
+                series,
+                request,
+                articleCount.intValue() + 1
+        );
+        this.articleService.save(article);
 
-    String thumbnailKey = this.articleService.uploadThumbnailImage(
-      thumbnail,
-      request.seriesId(),
-      article.getId()
-    );
+        String thumbnailKey = this.articleService.uploadThumbnailImage(
+                thumbnail,
+                request.seriesId(),
+                article.getId()
+        );
 
-    article.changeThumbnailKey(thumbnailKey);
+        article.changeThumbnailKey(thumbnailKey);
 
-    return new ArticlePost.Response(article.getId());
-  }
+        return new ArticlePost.Response(article.getId());
+    }
 
 }
--- src/main/java/com/prgrms/monthsub/module/series/article/app/ArticleController.java
+++ src/main/java/com/prgrms/monthsub/module/series/article/app/ArticleController.java
@@ -5,8 +5,6 @@ import com.prgrms.monthsub.common.jwt.JwtAuthentication;
 import com.prgrms.monthsub.module.series.article.dto.ArticlePost;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
-import javax.validation.Valid;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,28 +13,33 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.io.IOException;
+
 @RestController
 @Tag(name = "Article")
 public class ArticleController {
 
-  private final ArticleAssemble articleAssemble;
+    private final ArticleAssemble articleAssemble;
 
-  public ArticleController(
-    ArticleAssemble articleAssemble
-  ) {this.articleAssemble = articleAssemble;}
+    public ArticleController(
+            ArticleAssemble articleAssemble
+    ) {
+        this.articleAssemble = articleAssemble;
+    }
 
-  @PostMapping(path = "/article", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-  @Operation(summary = "아티클 작성")
-  @Tag(name = "[화면]-아티클")
-  public ApiResponse<ArticlePost.Response> postArticle(
-    @AuthenticationPrincipal JwtAuthentication authentication,
-    @RequestPart(required = false) MultipartFile thumbnail,
-    @Valid @RequestPart ArticlePost.Request request
-  ) throws IOException {
-    return ApiResponse.ok(
-      HttpMethod.POST,
-      articleAssemble.createArticle(thumbnail, request)
-    );
-  }
+    @PostMapping(path = "/article", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "아티클 작성")
+    @Tag(name = "[화면]-아티클")
+    public ApiResponse<ArticlePost.Response> postArticle(
+            @AuthenticationPrincipal JwtAuthentication authentication,
+            @RequestPart MultipartFile thumbnail,
+            @Valid @RequestPart ArticlePost.Request request
+    ) throws IOException {
+        return ApiResponse.ok(
+                HttpMethod.POST,
+                articleAssemble.createArticle(thumbnail, request)
+        );
+    }
 
 }
