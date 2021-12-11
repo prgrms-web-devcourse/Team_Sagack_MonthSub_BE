a7a990c5262bf507189e4d78309d0c0d5ca77b98
eunseo2 <20180972@sungshin.ac.kr>
2021-12-11 18:52:12 +0900

    [MON-149] feat : uploadDate Seed 데이터

---
+++ src/main/resources/db/seed/R__article_upload_date_seed.sql
@@ -0,0 +1,28 @@
+INSERT INTO article_upload_date (day, series_id)
+VALUES('MONDAY',1),
+      ('FRIDAY',1),
+      ('MONDAY',2),
+      ('FRIDAY',2),
+      ('TUESDAY',3),
+      ('FRIDAY',3),
+      ('TUESDAY',4),
+      ('FRIDAY',4),
+      ('WEDNESDAY',5),
+      ('FRIDAY',5),
+      ('WEDNESDAY',6),
+      ('FRIDAY',6),
+      ('MONDAY',7),
+      ('FRIDAY',7),
+      ('MONDAY',8),
+      ('FRIDAY',8),
+      ('MONDAY',9),
+      ('SUNDAY',9),
+      ('MONDAY',10),
+      ('SUNDAY',10),
+      ('MONDAY',11),
+      ('FRIDAY',11),
+      ('SATURDAY',11),
+      ('MONDAY',12),
+      ('SUNDAY',12);
+
+
