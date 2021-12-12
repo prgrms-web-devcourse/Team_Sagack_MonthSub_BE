c5692d020ef1ba072c63eaa20c424c84b7781134
youji <dbwlgna98@naver.com>
2021-12-12 17:49:03 +0900

    fix : 아티클 이미지 edit API 수정

--- src/main/java/com/prgrms/monthsub/module/series/article/app/ArticleController.java
+++ src/main/java/com/prgrms/monthsub/module/series/article/app/ArticleController.java
@@ -10,7 +10,6 @@ import javax.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -65,7 +64,7 @@ public class ArticleController {
     return this.articleAssemble.getArticleOne(id);
   }
 
-  @PatchMapping(path = "/{id}/thumbnail", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+  @PutMapping(path = "/{id}/thumbnail", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
   @Operation(summary = "아티클 썸네일 이미지 업데이트")
   @Tag(name = "[화면]-아티클")
   public String registerImage(
