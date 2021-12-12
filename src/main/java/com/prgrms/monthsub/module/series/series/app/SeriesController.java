b368e608d725dd726f2dc5bf863d66815203c115
youji <dbwlgna98@naver.com>
2021-12-12 17:07:25 +0900

    fix : 시리즈 이미지 수정 API 수정

--- src/main/java/com/prgrms/monthsub/module/series/series/app/SeriesController.java
+++ src/main/java/com/prgrms/monthsub/module/series/series/app/SeriesController.java
@@ -15,10 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -97,12 +97,12 @@ public class SeriesController {
   public SeriesSubscribeEdit.Response editSeries(
     @AuthenticationPrincipal JwtAuthentication authentication,
     @PathVariable Long id,
-    @Valid @RequestPart SeriesSubscribeEdit.Request request
+    @Valid @RequestBody SeriesSubscribeEdit.Request request
   ) {
     return this.seriesAssemble.editSeries(id, request);
   }
 
-  @PatchMapping(path = "/{id}/thumbnail", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+  @PutMapping(path = "/{id}/thumbnail", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
   @Operation(summary = "시리즈 썸네일 이미지 업데이트")
   @Tag(name = "[화면]-시리즈")
   public String registerImage(
