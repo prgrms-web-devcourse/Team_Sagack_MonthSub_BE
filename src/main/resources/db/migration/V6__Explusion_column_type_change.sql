aa668e7fffc79530a0f4a7b25b5d8911985ac8b9
0923kdh <0923kdh@gmail.com>
2021-12-13 17:44:25 +0900

    [MON-169] chore:  Explusion 필드 타입 변경

---
+++ src/main/resources/db/migration/V6__Explusion_column_type_change.sql
@@ -0,0 +1,4 @@
+ALTER table expulsion modify status varchar(50);
+ALTER table expulsion modify file_category varchar(50);
+ALTER table expulsion modify domain_type varchar(50);
+ALTER table expulsion modify file_type varchar(50);
