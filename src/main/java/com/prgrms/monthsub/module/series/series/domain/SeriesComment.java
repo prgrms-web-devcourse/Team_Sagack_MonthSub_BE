382bdad730c9bedef78b8cd13ecae27ce878cc2b
0923kdh <0923kdh@gmail.com>
2022-01-18 16:48:05 +0900

    [MON-291] feat : 시리즈 댓글 테이블 설계

--- src/main/java/com/prgrms/monthsub/module/series/series/domain/SeriesComment.java
+++ src/main/java/com/prgrms/monthsub/module/series/series/domain/SeriesComment.java
@@ -1,6 +1,7 @@
 package com.prgrms.monthsub.module.series.series.domain;
 
 import com.prgrms.monthsub.common.domain.BaseEntity;
+import com.prgrms.monthsub.module.part.user.domain.User;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -20,43 +22,53 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "comment")
-public class Comment extends BaseEntity {
+@Table(name = "series_comment")
+public class SeriesComment extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", columnDefinition = "BIGINT")
   private Long id;
 
+  @Column(name = "contents", columnDefinition = "VARCHAR(300)", nullable = false)
+  private String contents;
+
   @Column(name = "user_id", columnDefinition = "BIGINT", nullable = false)
   private Long userId;
 
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "series_id", referencedColumnName = "id")
-  private Series series;
+  @Column(name = "series_id", columnDefinition = "BIGINT", nullable = false)
+  private Long seriesId;
 
-  @Column(name = "contents", columnDefinition = "VARCHAR(200)", nullable = false)
-  private String contents;
+  @Column(name = "parent_id", columnDefinition = "BIGINT")
+  private Long parentId;
+
+  @Column(name = "sequence", columnDefinition = "INT", nullable = false)
+  private int sequence;
 
   @Enumerated(EnumType.STRING)
   @Column(name = "comment_status", columnDefinition = "VARCHAR(50)", nullable = false)
   private CommentStatus commentStatus;
 
   @Builder
-  private Comment(
-    Long userId,
-    Series series,
+  private SeriesComment(
     String contents,
+    Long userId,
+    Long seriesId,
+    Long parentId,
+    int sequence,
     CommentStatus commentStatus
   ) {
-    this.userId = userId;
-    this.series = series;
     this.contents = contents;
+    this.userId = userId;
+    this.seriesId = seriesId;
+    this.parentId = parentId;
+    this.sequence = sequence;
     this.commentStatus = commentStatus;
   }
 
   public enum CommentStatus {
     CREATED,
+    MODIFIED,
     DELETED;
 
     public static CommentStatus of(String commentStatus) {
