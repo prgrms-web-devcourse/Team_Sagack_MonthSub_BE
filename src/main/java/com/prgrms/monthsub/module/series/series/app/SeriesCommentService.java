28c40943a12af0fa3f324037e7f4ea405bc0b845
0923kdh <0923kdh@gmail.com>
2022-01-25 15:30:40 +0900

    [MON-301] 댓글, 대댓글 조회 api

--- src/main/java/com/prgrms/monthsub/module/series/series/app/SeriesCommentService.java
+++ src/main/java/com/prgrms/monthsub/module/series/series/app/SeriesCommentService.java
@@ -1,10 +1,17 @@
 package com.prgrms.monthsub.module.series.series.app;
 
+import com.prgrms.monthsub.module.part.user.app.provider.UserProvider;
+import com.prgrms.monthsub.module.part.user.domain.User;
 import com.prgrms.monthsub.module.series.series.converter.SeriesCommentConverter;
 import com.prgrms.monthsub.module.series.series.domain.SeriesComment;
 import com.prgrms.monthsub.module.series.series.domain.exception.SeriesException.SeriesCommentNotFound;
 import com.prgrms.monthsub.module.series.series.dto.SeriesCommentEdit;
+import com.prgrms.monthsub.module.series.series.dto.SeriesCommentList;
 import com.prgrms.monthsub.module.series.series.dto.SeriesCommentPost;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +22,36 @@ public class SeriesCommentService {
 
   private final SeriesCommentRepository seriesCommentRepository;
   private final SeriesCommentConverter seriesCommentConverter;
+  private final UserProvider userProvider;
 
   public SeriesCommentService(
     SeriesCommentRepository seriesCommentRepository,
-    SeriesCommentConverter seriesCommentConverter
+    SeriesCommentConverter seriesCommentConverter,
+    UserProvider userProvider
   ) {
     this.seriesCommentRepository = seriesCommentRepository;
     this.seriesCommentConverter = seriesCommentConverter;
+    this.userProvider = userProvider;
+  }
+
+  public SeriesCommentList.Response getComments(
+    Optional<Long> userIdOrEmpty,
+    Long seriesId
+  ) {
+    List<SeriesComment> comments = this.seriesCommentRepository.findAllBySeriesIdAndParentIdIsNull(
+      seriesId
+    );
+    List<SeriesComment> replyComments = this.seriesCommentRepository.findAllBySeriesIdAndParentIdIsNotNull(
+      seriesId
+    );
+
+    List<Long> userIds = Stream.concat(comments.stream(), replyComments.stream())
+      .map(SeriesComment::getUserId)
+      .distinct()
+      .collect(Collectors.toList());
+    List<User> users = this.userProvider.findByIdIn(userIds);
+
+    return this.seriesCommentConverter.toResponse(userIdOrEmpty, comments, replyComments, users);
   }
 
   public SeriesCommentPost.Response saveComment(
@@ -42,7 +72,7 @@ public class SeriesCommentService {
   ) {
     SeriesComment seriesComment = this.seriesCommentRepository.findById(request.id())
       .orElseThrow(() -> new SeriesCommentNotFound("id= " + request.id()));
-    if(!seriesComment.isMine(userId)){
+    if (!seriesComment.isMine(userId)) {
       throw new AccessDeniedException("수정 권한이 없습니다.");
     }
 
@@ -54,14 +84,17 @@ public class SeriesCommentService {
       .build();
   }
 
-  public void deleteComment(Long userId, Long id){
+  public void deleteComment(
+    Long userId,
+    Long id
+  ) {
     SeriesComment seriesComment = this.seriesCommentRepository.findById(id)
       .orElseThrow(() -> new SeriesCommentNotFound("id= " + id));
-    if(!seriesComment.isMine(userId)){
+    if (!seriesComment.isMine(userId)) {
       throw new AccessDeniedException("삭제 권한이 없습니다.");
     }
 
     seriesComment.deleteComment();
   }
-  
+
 }
--- src/main/java/com/prgrms/monthsub/module/series/series/converter/SeriesCommentConverter.java
+++ src/main/java/com/prgrms/monthsub/module/series/series/converter/SeriesCommentConverter.java
@@ -1,26 +1,107 @@
 package com.prgrms.monthsub.module.series.series.converter;
 
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.groupingBy;
+
+import com.prgrms.monthsub.module.part.user.domain.User;
 import com.prgrms.monthsub.module.series.series.domain.SeriesComment;
 import com.prgrms.monthsub.module.series.series.domain.SeriesComment.CommentStatus;
+import com.prgrms.monthsub.module.series.series.dto.SeriesCommentList;
+import com.prgrms.monthsub.module.series.series.dto.SeriesCommentList.CommentMetaInfoObject;
+import com.prgrms.monthsub.module.series.series.dto.SeriesCommentList.CommentObject;
+import com.prgrms.monthsub.module.series.series.dto.SeriesCommentList.ReplyCommentObject;
+import com.prgrms.monthsub.module.series.series.dto.SeriesCommentList.UserInfoObject;
 import com.prgrms.monthsub.module.series.series.dto.SeriesCommentPost;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SeriesCommentConverter {
 
-  public SeriesCommentConverter(){
+  public SeriesCommentConverter() {
   }
 
   public SeriesComment toEntity(
     Long userId,
     SeriesCommentPost.Request request
-  ){
+  ) {
     return SeriesComment.builder()
       .comment(request.comment())
       .userId(userId)
       .seriesId(request.seriesId())
       .parentId(request.parentId())
       .commentStatus(CommentStatus.CREATED)
+      .build();
+  }
+
+  public SeriesCommentList.Response toResponse(
+    Optional<Long> userIdOrEmpty,
+    List<SeriesComment> comments,
+    List<SeriesComment> replyComments,
+    List<User> users
+  ) {
+    Map<Long, User> usersInfo = users.stream()
+      .collect(Collectors.toMap(User::getId, identity()));
+
+    Map<Long, List<SeriesComment>> replyCommentsInfo = replyComments.stream()
+      .collect(groupingBy(SeriesComment::getParentId));
+
+    List<CommentObject> commentObjects = comments.stream().map(comment -> {
+        User user = usersInfo.get(comment.getUserId());
+
+        UserInfoObject commentUserInfoObject = UserInfoObject.builder()
+          .nickname(user.getNickname())
+          .profileImage(user.getProfileKey())
+          .build();
+
+        CommentMetaInfoObject commentMetaInfoObject = CommentMetaInfoObject.builder()
+          .isMine(userIdOrEmpty.map(userId -> {
+              return userId.equals(comment.getUserId());
+            }
+          ).orElse(false))
+          .isDeleted(comment.getCommentStatus() == CommentStatus.DELETED)
+          .updatedDate(comment.getUpdateAt().toLocalDate())
+          .build();
+
+        List<SeriesComment> replySeriesComment = replyCommentsInfo.get(comment.getId());
+        List<ReplyCommentObject> replyCommentObject = replySeriesComment.stream()
+          .map(
+            replyComment -> {
+              User replyCommentUser = usersInfo.get(replyComment.getUserId());
+              UserInfoObject replyCommentUserInfoObject = UserInfoObject.builder()
+                .nickname(replyCommentUser.getNickname())
+                .profileImage(replyCommentUser.getProfileKey())
+                .build();
+
+              CommentMetaInfoObject replyCommentMetaInfoObject = CommentMetaInfoObject.builder()
+                .isMine(userIdOrEmpty.map(userId -> {
+                    return userId.equals(replyComment.getUserId());
+                  }
+                ).orElse(false))
+                .isDeleted(replyComment.getCommentStatus() == CommentStatus.DELETED)
+                .updatedDate(replyComment.getUpdateAt().toLocalDate())
+                .build();
+
+              return ReplyCommentObject.builder()
+                .userInfoObject(replyCommentUserInfoObject)
+                .commentMetaInfoObject(replyCommentMetaInfoObject)
+                .build();
+            }
+          ).collect(Collectors.toList());
+
+        return CommentObject.builder()
+          .userInfoObject(commentUserInfoObject)
+          .commentMetaInfoObject(commentMetaInfoObject)
+          .replyCommentObjects(replyCommentObject)
+          .build();
+      }
+    ).collect(Collectors.toList());
+
+    return SeriesCommentList.Response.builder()
+      .commentObjects(commentObjects)
       .build();
   }
 
